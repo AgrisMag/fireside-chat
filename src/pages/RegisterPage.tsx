99dcd73afa005ca3071e96c4b296955fc6823c1e
Agris <agris.magdalenoks@gmail.com>
2023-09-23 18:42:11 +0300

    addded register page

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,15 @@
+export const RegisterPage = () => {
+  return (
+    <div>
+      <label htmlFor="">
+        <input type="text" />
+      </label>
+      <label htmlFor="">
+        <input type="email" name="" id="" />
+      </label>
+      <label htmlFor="">
+        <input type="password" name="" id="" />
+      </label>
+    </div>
+  );
+};
